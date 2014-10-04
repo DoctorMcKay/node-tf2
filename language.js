ca39de20e0935e0fdc84e0f7218cd2b28ab33e9f
Alexander Corn <mckay@doctormckay.com>
2014-10-04 03:48:23 -0400

    Added EGCBaseClientMsg to language

---
+++ language.js
@@ -0,0 +1,13 @@
+// This file contains representations of message IDs
+
+module.exports = {
+	// EGCBaseClientMsg
+	PingRequest: 3001,
+	PingResponse: 3002,
+	ClientWelcome: 4004,
+	ServerWelcome: 4005,
+	ClientHello: 4006,
+	ServerHello: 4007,
+	ClientGoodbye: 4008,
+	ServerGoodbye: 4009
+};
