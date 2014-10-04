7486893f228d0ffc5332aaeb4cf21d80bf2ceba5
Alexander Corn <mckay@doctormckay.com>
2014-10-04 06:01:38 -0400

    Actually add that protos.js file

---
+++ protos.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+var Schema = require('protobuf').Schema;
+
+module.exports = {
+	"base_gcmessages": new Schema(fs.readFileSync(__dirname + '/generated/base_gcmessages.desc')),
+	"tf_gcmessages": new Schema(fs.readFileSync(__dirname + '/generated/tf_gcmessages.desc'))
+};
