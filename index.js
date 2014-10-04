9b5a1a01459b10f300dd2861aa5237e203772b60
Alexander Corn <mckay@doctormckay.com>
2014-10-04 03:34:21 -0400

    Set up basic stuff for module

---
+++ index.js
@@ -0,0 +1,16 @@
+var vdf = require('vdf');
+
+// Prep protobuf stuff
+var Schema = require('protobuf').Schema;
+var base_gcmessages = new Schema(fs.readFileSync('./generated/base_gcmessages.desc'));
+var tf_gcmessages = new Schema(fs.readFileSync('./generated/tf_gcmessages.desc'));
+var protomask = 0x80000000;
+
+module.exports = TeamFortress2;
+
+require('util').inherits(TeamFortress2, require('events').EventEmitter);
+
+function TeamFortress2(steam) {
+	this._steam = steam;
+	this.connectedToGC = false;
+}
