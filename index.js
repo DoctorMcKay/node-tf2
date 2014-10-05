e1a6da6699a21fc70ae05e7488b9482164e046a8
Alexander Corn <mckay@doctormckay.com>
2014-10-05 02:50:36 -0400

    Added TradeResponse enum

--- index.js
+++ index.js
@@ -18,6 +18,35 @@ TeamFortress2.GCGoodbyeReason = {
 	NO_SESSION: 2
 };
 
+TeamFortress2.TradeResponse = {
+	Accepted: 0,
+	Declined: 1,
+	TradeBannedInitiator: 2,
+	TradeBannedTarget: 3,
+	TargetAlreadyTrading: 4,
+	Disabled: 5,
+	NotLoggedIn: 6,
+	Cancel: 7,
+	TooSoon: 8,
+	TooSoonPenalty: 9,
+	ConnectionFailed: 10,
+	AlreadyTrading: 11,
+	AlreadyHasTradeRequest: 12,
+	NoResponse: 13,
+	CyberCafeInitiator: 14,
+	CyberCafeTarget: 15,
+	SchoolLabInitiator: 16,
+	SchoolLabTarget: 16,
+	InitiatorBlockedTarget: 18,
+	InitiatorNeedsVerifiedEmail: 20,
+	InitiatorNeedsSteamGuard: 21,
+	TargetAccountCannotTrade: 22,
+	InitiatorSteamGuardDuration: 23,
+	InitiatorPasswordResetProbation: 24,
+	InitiatorNewDeviceCooldown: 25,
+	OKToDeliver: 50
+};
+
 function TeamFortress2(steam) {
 	this._steam = steam;
 	this.haveGCSession = false;
