f207ca10fa2d7dc80549ff0424228cfab0e4c01d
Alexander Corn <mckay@doctormckay.com>
2014-10-05 01:24:33 -0400

    Parse gcsdk_gcmessages.desc

--- protos.js
+++ protos.js
@@ -3,5 +3,6 @@ var Schema = require('protobuf').Schema;
 
 module.exports = {
 	"base_gcmessages": new Schema(fs.readFileSync(__dirname + '/generated/base_gcmessages.desc')),
+	"gcsdk_gcmessages": new Schema(fs.readFileSync(__dirname + '/generated/gcsdk_gcmessages.desc')),
 	"tf_gcmessages": new Schema(fs.readFileSync(__dirname + '/generated/tf_gcmessages.desc'))
 };
