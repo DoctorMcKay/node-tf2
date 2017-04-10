b4b8225256fd00bce576c098d4a9feb0c3d1f96f
Alexander Corn <mckay@doctormckay.com>
2017-04-10 13:01:47 -0400

    Added debug output for handled GC messages

--- index.js
+++ index.js
@@ -21,21 +21,23 @@ function TeamFortress2(steam) {
 	var self = this;
 	
 	this._gc.on('message', function(header, body, callback) {
-		var protobuf = !!header.proto;
-		
-		if(self._handlers[header.msg]) {
-			self._handlers[header.msg].call(self, protobuf ? body : ByteBuffer.wrap(body, ByteBuffer.LITTLE_ENDIAN));
-		} else {
-			var msgName = header.msg;
-			for(var i in Language) {
-				if(Language[i] == header.msg) {
-					msgName = i;
-					break;
-				}
-			}
-			
-			self.emit('debug', "Got unhandled GC message " + msgName + (protobuf ? " (protobuf)" : ""));
-		}
+        var protobuf = !!header.proto;
+        var handled = false;
+
+        if (self._handlers[header.msg]) {
+            handled = true;
+            self._handlers[header.msg].call(self, protobuf ? body : ByteBuffer.wrap(body, ByteBuffer.LITTLE_ENDIAN));
+        }
+
+        var msgName = header.msg;
+        for (var i in Language) {
+            if(Language.hasOwnProperty(i) && Language[i] == header.msg) {
+                msgName = i;
+                break;
+            }
+        }
+
+        self.emit('debug', "Got " + (handled ? "handled" : "unhandled") + " GC message " + msgName + (protobuf ? " (protobuf)" : ""));
 	});
 
 	// "extend" the default steam.gamesPlayed function so we can catch when TF2 starts up
